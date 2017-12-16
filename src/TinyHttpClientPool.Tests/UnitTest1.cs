ddde7c8114c65eac96576b93075fc5aac891b051
Johan Karlsson <johan.kson@gmail.com>
2017-12-16 16:44:26 +0100

    Added initial tests

--- src/TinyHttpClientPool.Tests/UnitTest1.cs
+++ src/TinyHttpClientPool.Tests/UnitTest1.cs
@@ -1,14 +1,52 @@
 using System;
+using TinyHttpClientPoolLib;
 using Xunit;
 
 namespace TinyHttpClient.Tests
 {
-    public class UnitTest1
+  
+    public class TinyHttpClientTest
     {
         [Fact]
-        public void Test1()
+        public void InitializationTest()
         {
+            // Arrange
+            var pool = new TinyHttpClientPool();
 
+            // Act
+            var a = pool.Fetch();
+
+            // Assert
+            Assert.NotNull(a);
+        }
+
+        [Fact]
+        public void NewInstanceTest()
+        {
+            // Arrange
+            var pool = new TinyHttpClientPool();
+
+            // Act
+            var a = pool.Fetch();
+            var b = pool.Fetch();
+
+            // Assert
+            Assert.NotSame(a, b);
+        }
+
+        [Fact]
+        public void ReuseTest()
+        {
+            // Arrange
+            var pool = new TinyHttpClientPool();
+
+            // Act
+            var a = pool.Fetch();
+            a.Dispose();
+            var b = pool.Fetch();
+
+            // Assert
+            Assert.Same(a, b);
         }
     }
 }
